187d316d3770da1185359c1a2fef2de372f3bef5
Shilpa Bindhu <shilpamaalootty@gmail.com>
2023-05-19 22:41:59 +0200

    fixed validation

--- js/contact.js
+++ js/contact.js
@@ -5,9 +5,63 @@ var validateForm = function () {
     ev.preventDefault()
     ev.stopPropagation()
 
-    form.classList.add('was-validated')
+    var error = false;
 
-    if (!form.checkValidity()) {
+    var firstNameInput = document.getElementById('form-f-name');
+    var firstName = firstNameInput.value.trim();
+    var firstNameError = document.querySelector('#form-f-name + .invalid-feedback');
+    firstNameInput.classList.remove('is-valid', 'is-invalid');
+    if (!(/^[a-z ]+$/i.test(firstName))) {
+      error = true;
+      firstNameError.innerHTML = 'Invalid firstName';
+      firstNameInput.classList.add('is-invalid');
+    } else {
+      firstNameError.innerHTML = '';
+      firstNameInput.classList.add('is-valid');
+    }
+
+    var lastNameInput = document.getElementById('form-l-name');
+    var lastName = lastNameInput.value.trim();
+    var lastNameError = document.querySelector('#form-l-name + .invalid-feedback');
+    lastNameInput.classList.remove('is-valid', 'is-invalid');
+    if(!(/^[a-z ]+$/i.test(lastName))) {
+      error = true;
+      lastNameError.innerHTML = 'Invalid lastName';
+      lastNameInput.classList.add('is-invalid');
+    } else {
+      lastNameError.innerHTML = '';
+      lastNameInput.classList.add('is-valid');
+    }
+    var emailInput = document.getElementById('form-email');
+    let email = emailInput.value.trim();
+    var emailError = document.querySelector('#form-email + .invalid-feedback');
+    emailError.innerHTML = '';
+    emailInput.classList.remove('is-valid','is-invalid');
+    if(!(/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(email))) {
+      error = true;
+      emailError.innerHTML = 'Invalid Email';
+      emailInput.classList.add('is-invalid');
+    } else {
+      emailError.innerHTML = '';
+      emailInput.classList.add('is-valid');
+    }
+
+    var contactReasonInput = document.getElementById('form-message');
+    var contactReason = contactReasonInput.value.trim();
+    var contactReasonError = document.querySelector('#form-message + .invalid-feedback');
+    contactReasonError.innerHTML = '';
+    contactReasonInput.classList.remove('is-valid','is-invalid');
+    if(contactReason.length < 5){
+      contactReasonError.innerHTML = 'Invalid contact reason';
+      contactReasonInput.classList.add('is-invalid');
+      error = true;
+    } else {
+      error = false;
+      contactReason.innerHTML = '';
+      contactReasonInput.classList.add('is-valid');
+    }
+
+    if (error) {
       return;
     }
 
---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+let firstName = document.getElementById('firstName');
+
+let submit = document.getElementById('submit').addEventListener('click',function() {
+
+})
+alert(firstName);
+
+let z = 10; // global 
+
+{
+    let y = 10;
+}
+
+
+function a() {
+    let x= 20;
+}
